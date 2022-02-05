a50fecae59fc6a5cec6f1e689f69be261c11809a
DESKTOP-99CVDAU\User_12 <gamiraude@gmail.com>
2022-02-05 12:12:05 +0100

    Add project files.

---
+++ Predavanje6/Predavanje6/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace Predavanje6
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            GestureLabel.Text = "Xamarin";
+        }
+
+        private void TapGestureRecognizer_Tapped_1(object sender, EventArgs e)
+        {
+            LinkLabel.TextColor = Color.Purple;
+            Browser.OpenAsync("https://google.com");
+        }
+    }
+}
